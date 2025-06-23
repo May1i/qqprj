88a8b91f3b54ceb2deab387f42a604380e44fd73
May1i <1803017756@qq.com>
2025-06-23 17:05:27 +0800

    none

--- search_friend.cpp
+++ search_friend.cpp
@@ -23,32 +23,32 @@ void search_friend::on_searchBtn_clicked()
 
 void search_friend::addUserToList(const QString &username, const QString &account,const QPixmap &icon)
 {
-    //防止进行重复添加同一个用户
-//    bool userExists = false;
-//        for (int i = 0; i < ui->listWidget->count(); ++i)
-//        {
-//            QListWidgetItem *item = ui->listWidget->item(i);
-//            QWidget *widget = ui->listWidget->itemWidget(item);
-//            if (widget)
-//            {
-//                // 获取用户名和账号（假设你的 QLabel 格式是 "用户名\n账号"）
-//                QLabel *textLabel = widget->findChild<QLabel*>();
-//                if (textLabel)
-//                {
-//                    QStringList parts = textLabel->text().split("\n");
-//                    if (parts.size() >= 2 && parts[1] == account)
-//                    { // 检查账号是否相同
-//                        userExists = true;
-//                        break;
-//                    }
-//                }
-//            }
-//        }
-//        if (userExists)
-//        {
-//            qDebug() << "用户已存在，不重复添加：" << username << account;
-//            return;
-//        }
+//    防止进行重复添加同一个用户
+    //    bool userExists = false;
+    //        for (int i = 0; i < ui->listWidget->count(); ++i)
+    //        {
+    //            QListWidgetItem *item = ui->listWidget->item(i);
+    //            QWidget *widget = ui->listWidget->itemWidget(item);
+    //            if (widget)
+    //            {
+    //                // 获取用户名和账号
+    //                QLabel *textLabel = widget->findChild<QLabel*>();
+    //                if (textLabel)
+    //                {
+    //                    QStringList parts = textLabel->text().split("\n");
+    //                    if (parts.size() >= 2 && parts[1] == account)
+    //                    { // 检查账号是否相同
+    //                        userExists = true;
+    //                        break;
+    //                    }
+    //                }
+    //            }
+    //        }
+    //        if (userExists)
+    //        {
+    //            qDebug() << "用户已存在，不重复添加：" << username << account;
+    //            return;
+    //        }
 
 
     // 创建列表项
@@ -69,12 +69,15 @@ void search_friend::addUserToList(const QString &username, const QString &accoun
     painter.drawPixmap(0, 0, icon.scaled(40, 40, Qt::KeepAspectRatio));
 
     iconLabel->setPixmap(circularPixmap);
-    // 添加文字（用户名 + 账号）
+    // 添加文字（用户名  账号）
     QLabel *textLabel = new QLabel();
-    textLabel->setText(QString("%1\n%2").arg(username).arg(account));
+    textLabel->setText(QString("%1\t%2").arg(username).arg(account));
+    QPushButton *textAddBtn=new QPushButton();
+    textAddBtn->setText("添加好友");
     // 组合布局
     layout->addWidget(iconLabel);
     layout->addWidget(textLabel);
+    layout->addWidget(textAddBtn);
     layout->setContentsMargins(5, 5, 5, 5);
     // 设置列表项
     item->setSizeHint(itemWidget->sizeHint());
